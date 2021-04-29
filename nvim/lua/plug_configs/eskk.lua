8c856bf92c28ee09d0c71cf3eff1baf9ef0a544b
high-moctane <high.moctane@gmail.com>
2021-04-29 17:57:45 +0900

    環境依存のバグ修正

--- nvim/lua/plug_configs/eskk.lua
+++ nvim/lua/plug_configs/eskk.lua
@@ -9,7 +9,7 @@ local large_dictionary_path = skk_dir .. "/SKK-JISYO.total"
 
 M.config = function()
     vim.g["eskk#large_dictionary"] = {
-        path = "/Users/moctane/.local/share/skk/SKK-JISYO.total",
+        path = vim.fn.expand("~/.local/share/skk/SKK-JISYO.total"),
         sorted = 1,
         encoding = "euc-jp",
     }
