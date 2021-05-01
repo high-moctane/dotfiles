9df571465509559c37525175224f3601c129c6aa
high-moctane <high.moctane@gmail.com>
2021-05-02 02:20:00 +0900

    変なエスケープを取り除いた

--- home/shell_common.sh
+++ home/shell_common.sh
@@ -13,8 +13,8 @@ fi
 if [[ $(uname) == "Linux" ]]; then
     test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
     test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-    test -r ~/.bash_profile && eval \$($(brew --prefix)/bin/brew shellenv)
-    eval \$($(brew --prefix)/bin/brew shellenv)
+    test -r ~/.bash_profile && eval $($(brew --prefix)/bin/brew shellenv)
+    eval $($(brew --prefix)/bin/brew shellenv)
 fi
 
 # Language Setup
